988f84795e2ed543de852ec33005ddf5f33c95f4
zwaters <zak.waters@student.qut.edu.au>
2014-12-08 13:06:48 +1000

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,12 +1,11 @@
 import json
 
-json_data_path = "C:/Users/Annette/Desktop/AskHistoriansJSON/"
+#using static paths at the moment, will become dynamic in future commits
+json_data_path = ""
 json_files =[
-               'did_any_of_leonardo_da_vincis_research_actually.json',
-               'how_were_individual_ss_soldiers_treated_by_the.json',
-               'its_year_xxxx_of_your_specialty_a_dead_body_is.json',
-               'was_there_intellectual_exchange_between_ancient.json',
-               'what_is_fascism.json'
+            "file1",
+            "file2",
+            "etc"
             ]
 
 for file in json_files:
